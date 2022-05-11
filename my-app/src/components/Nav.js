35adfb522a7abc40df62d85eb70e6e0ac71bb21a
JosephShin <joespam9999@gmail.com>
2022-05-11 11:31:14 -0400

    merge with logout

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, Navbar} from 'react-bootstrap'
 
 function Nav({}) {
   return (
-    <nav>
-      <NavLink exact to="/">
-        Home{" "}
-      </NavLink>
-      <NavLink to="/about">About </NavLink>
-      <NavLink to="/restaurants">Restaurants</NavLink>
-      <NavLink to="/signin">Sign In </NavLink>
-      <NavLink to="/signup">Sign Up</NavLink>
-    </nav>
+    <Container>
+      <Navbar.Brand href="/">Home</Navbar.Brand>
+      <Navbar.Brand href="/about">About</Navbar.Brand>
+      <Navbar.Brand href="/restaurants">Restaurants</Navbar.Brand>
+      <Navbar.Brand href="/signin">Sign In</Navbar.Brand>
+      <Navbar.Brand href="/signup">Sign Up</Navbar.Brand>
+      {/* <nav>
+        <NavLink exact to="/">
+          Home{" "}
+        </NavLink>
+        <NavLink to="/about">About </NavLink>
+        <NavLink to="/restaurants">Restaurants</NavLink>
+        <NavLink to="/signin">Sign In </NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </nav> */}
+    </Container>
   );
 }
 export default Nav;
