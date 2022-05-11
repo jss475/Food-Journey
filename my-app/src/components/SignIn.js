a5f485dae76ac5cbb6abc636056fee77be05e5a7
Hyun Sung Cho <hcho774@gmail.com>
2022-05-11 16:36:05 -0400

    signinup background updated

--- my-app/src/components/SignIn.js
+++ my-app/src/components/SignIn.js
@@ -2,23 +2,25 @@ import React from "react";
 
 function SignIn({ handleSignInSubmit }) {
   return (
-    <div className="loginbox">
-      <h3>Please Sign In!</h3>
-      <form id="sign_in_form" onSubmit={handleSignInSubmit}>
-        <p>Username</p>
-        <input
-          name="username"
-          type="username"
-          placeholder="Enter your username"
-        />
-        <p>Password</p>
-        <input
-          name="password"
-          type="password"
-          placeholder="Enter your password"
-        />
-        <button type="submit">Submit</button>
-      </form>
+    <div id="sign_in_bg">
+      <div className="loginbox">
+        <h3>Please Sign In!</h3>
+        <form id="sign_in_form" onSubmit={handleSignInSubmit}>
+          <p>Username</p>
+          <input
+            name="username"
+            type="username"
+            placeholder="Enter your username"
+          />
+          <p>Password</p>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
